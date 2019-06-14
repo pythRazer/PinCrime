2da18fc3204e32e41279bf24f183251e394c64ec
Jui Chi Lee <pythrazer@gmail.com>
2019-06-14 10:26:35 +0900

    change

--- ByeByeCrime.py
+++ ByeByeCrime.py
@@ -2,21 +2,23 @@ import pandas
 import json
 from geopy.geocoders import ArcGIS
 import folium
+import sys
+import urllib
+import os
 import numpy
+
+
 
 while True:
 
-    input_method = input("Address for 1, latitude and longitude for 2: ")
 
-    # if(input_method != "1" or input_method != "2"):
-    #     print("Invalid input, please try again")
-    #     continue
+    input_method = input("Address for 1, latitude and longitude for 2, close the app for 3: ")
 
     if(input_method == "1"):
         try:
             user_address = input("Please enter the address: ")
             geolocator = ArcGIS(user_agent="ByeByeCrime")
-            location = geolocator.geocode(user_address)
+            location = geolocator.geocode("UK" + user_address)
             print(location.address)
             print(location.latitude, location.longitude)
             user_lat = str(location.latitude)
@@ -30,14 +32,30 @@ while True:
         try:
             user_lat = input("Please enter the latitude: ")
             user_lon = input("Please enter the longitude: ")
-            if(user_lat.isdigit() or user_lon.isdigit() != True):
-                print("Invalid latitude or longitude, please try again")
+            if(user_lat.isdigit() != True or user_lon.isdigit() != True):
+                print("Invalid latitude or longitude, please try again lll")
                 continue
         except:
             print("Invalid latitude or longitude, please try again")
             continue
         else:
             break
+
+    elif(input_method == "3"):
+        print("Closed")
+        sys.exit()
+
+    else:
+        print("Invalid input, please try again")
+        continue
+
+
+
+
+    # if(input_method != "1" or input_method != "2"):
+    #     print("Invalid input, please try again")
+    #     continue
+
 
 
 print("Processing...")
@@ -61,8 +79,13 @@ crime_color = {
 
 
 def request(lat, lon):
-    df1_request = "https://data.police.uk/api/crimes-street/all-crime?lat=" + lat + "&" + "lng=" + lon
-    df1 = pandas.read_json(df1_request)
+    df1_request = "https://data.police.uk/api/crimes-street/all-crime?lat=" + str(lat) + "&" + "lng=" + str(lon)
+    try:
+        df1 = pandas.read_json(df1_request)
+    except:
+        print("")
+
+    print(df1["location"].size)
     lanlon(df1)
 
 
@@ -98,5 +121,15 @@ map = folium.Map(location=[user_lat, user_lon],
                  zoom_start=20,
                  titles="Crime Map")
 map.add_child(fg)
+
 map.save("ByeByeCrime.html")
+# path = os.path.abspath("ByeByeCrime.html")
+# page = urllib.request.urlopen(path).read()
+# print(page)
+# html_string = map.get_root().render()
+# f = codecs.open("ByeByeCrime.html", "r", "utf-8")
 print("The map was generated successfully")
+#36.84183000000007 -76.05180999999999
+#51.503659988314624 -0.11925004443394603
+
+
