e35cd359a06b7bcc18f0530f7d5cbf6b960166b7
Jui Chi Lee <pythrazer@gmail.com>
2019-07-06 17:40:25 +0900

    Backend finished

--- ByeByeCrime.py
+++ ByeByeCrime.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import pandas
-from geopy.geocoders import ArcGIS
+from geopy.geocoders import Nominatim
 import folium
 from folium.plugins import HeatMap
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 from textwrap import wrap
 
 crime_type = ['anti-social-behaviour', 'bicycle-theft', 'burglary', 'criminal-damage-arson', 'drugs', 'other-theft',
@@ -12,7 +12,6 @@ crime_type = ['anti-social-behaviour', 'bicycle-theft', 'burglary', 'criminal-da
 
 color = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen',
          'cadetblue', 'gray', 'pink', 'lightgreen']
-
 
 count = [0] * 14
 crime_color = {
@@ -44,30 +43,27 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/Statistics', methods=['GET'])
+def statistics():
+    return render_template('Statistics.html')
 
 
 @app.route('/result', methods=['POST', 'GET'])
 def byebyeresult():
     if request.method == 'POST':
         address = request.form
-        geolocator = ArcGIS(user_agent="app_test")
+        address = address['Address']
 
         # try:
+        geolocator = Nominatim(user_agent="app_test", timeout=10)
+        location = geolocator.geocode(address, addressdetails=True)
 
-
-        location = geolocator.geocode(address)
-        # location_extent = location.raw['extent']
-        # min_x = str(location_extent['xmin'])
-        # min_y = str(location_extent['ymin'])
-        # max_x = str(location_extent['xmax'])
-        # max_y = str(location_extent['ymax'])
-        # fb_return_url = "http://0.0.0.0:5000/result+minx" + min_x + "miny" + min_y + "max_x" + max_x + "max_y" + max_y
+        address_dict = location.raw['address']
+        city = address_dict['city']
 
         lat = str(location.latitude)
         lon = str(location.longitude)
         start_coords = (lat, lon)
-
-
 
         folium_map = folium.Map(location=start_coords, zoom_start=14, width='80%', height='80%')
         df_request = "https://data.police.uk/api/crimes-street/all-crime?lat=" + str(lat) + "&" + "lng=" + str(lon)
@@ -97,11 +93,9 @@ def byebyeresult():
                 if ctg == crime_type[j]:
                     count[j] += 1
 
-
         fgu = folium.FeatureGroup(name="User")
         fgu.add_child((folium.Marker(location=(lat, lon), tooltip="You are here",
                                      icon=folium.Icon(color='blue', icon='user'))))
-
 
         heat_map = HeatMap(data=crime_latlon, name='Contour map')
         folium_map.add_child(fg)
@@ -113,16 +107,29 @@ def byebyeresult():
         folium_map.save("templates/PinCrimes.html")
 
         crime_types = ['\n'.join(wrap(l, 12)) for l in crime_type]
-        plt.figure(figsize=(20, 10))
-        plt.bar(crime_types, count)
 
-        plt.savefig('static/new_plot.png')
+        fig = plt.figure(figsize=(20, 6))
+        x = range(14)
+
+        plt.bar(x, count)
+
+        plt.xticks(x, crime_types)
+
+        mpld3.plugins.clear(fig)
+
+        mpld3.save_html(fig, "templates/Statistics.html", no_extras=True, template_type="simple")
+
+        fb_url = "http://0.0.0.0:5000/result?" + city
+
+        fb_url = fb_url.replace(" ", "+")
+
+        print(fb_url)
 
         return render_template("result.html", location_address=location.address,
 
-                               total_crime=total_crime)
+                               total_crime=total_crime, city=city, url=fb_url)
     # except:
-        #     return render_template("error.html")
+    #     return render_template("error.html")
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,5 +1,94 @@
-# ByeByeCrime
-A crime map of UK, can filter the category, search location, etc. (still in progress)
+# Pin Crimes
 
-First look:
-![image](https://github.com/pythRazer/ByeByeCrime/blob/master/Screen%20Shot%202019-06-13%20at%2017.50.19.png)
+Pin Crimes is a web application which users can pick a place and check the crime rates with 14 categories around one miles in a month in UK. The map also shows the heatmap of the crime so that users can 
+see the distribution of it. The crime data is from Police API from UK police (https://data.police.uk/) .
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+pip install flask
+pip install pandas
+pip install geopy
+pip install folium
+pip install matplotlib
+pip install mpld3
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Execute ByeByeCrime.py and copy and paste the url to broser to see and test the web application
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Lee Jui Chi**
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
+
